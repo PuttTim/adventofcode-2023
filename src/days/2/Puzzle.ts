461a09cc878018fd66727d0ed6d3000fce0b3aea
Putt <57494734+PuttTim@users.noreply.github.com>
2023-12-02 18:57:22 +0800

    ✨ 2.2 pass

--- src/days/2/Puzzle.ts
+++ src/days/2/Puzzle.ts
@@ -62,6 +62,61 @@ const first = (input: string) => {
 const expectedFirstSolution = "solution 1"
 
 const second = (input: string) => {
+    // console.log("input2\n", input)
+
+    const games = input.replaceAll("\r", "").replaceAll(";", ",").split("\n")
+
+    console.log(games)
+
+    let gameSum = 0
+
+    games.forEach((game, index) => {
+        game = game.split(":")[1]
+
+        const max: TCubeType = {
+            red: 0,
+            green: 0,
+            blue: 0,
+        }
+
+        console.log(">>>", game)
+
+        game.split(",").forEach((pick) => {
+            // console.log(pick.split(/(\d+)/))
+            const cubes = pick.replaceAll("\n", "").split(/(\d+)/)
+
+            // console.log(typeof cubes[1], typeof cubes[2])
+
+            // console.log(limits[cubes[2].replaceAll(" ", "")] <= cubes[1])
+
+            // console.log(pick[1], pick[2])
+
+            max[cubes[2].replaceAll(" ", "")] = Math.max(
+                max[cubes[2].replaceAll(" ", "")],
+                parseInt(cubes[1]),
+            )
+        })
+
+        console.log(">>>MAX", max, max.red * max.green * max.blue)
+
+        // console.log(">>>BEFORE", gameIdSum)
+
+        // console.log(">>>VALID", valid, gameIdSum, index + 1)
+
+        // if (valid) {
+        // gameIdSum = gameIdSum + index + 1
+        // }
+
+        // console.log(">>>AFTER", gameIdSum)
+
+        // console.log(game, index + 1)
+        // console.log(gameIdSum)
+        const maxValue = max.red * max.green * max.blue
+        gameSum = gameSum + maxValue
+    })
+
+    console.log(">>>GAMESUM", gameSum)
+
     return "solution 2"
 }
 
